1362e3c37dde819d3aa0696e5fe4bd9b7bcfb8e7
abrao90 <anirudhrao2490@gmail.com>
2019-06-19 18:25:09 +0200

    sample

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hug A Pet`,
+    description: `Mobiler Tierartz | Hug A Pet - Deutschland`,
+    author: `Hug A Pet - Deutschland`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
